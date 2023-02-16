e4b3b8b404a47833f40752e4a5d78dba17470796
xitrolisax <mushroom.1661@gmail.com>
2023-02-16 16:00:49 -0600

    fix

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todolistamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
